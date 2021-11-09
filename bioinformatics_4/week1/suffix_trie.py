fc1eee53533ab28d3a180552a6f25d4bd3ffaee6
carrollpaul <57120760+TheBoilerman@users.noreply.github.com>
2021-11-09 10:33:05 -0800

    Add suffix tree

---
+++ bioinformatics_4/week1/suffix_trie.py
@@ -0,0 +1,13 @@
+import typing
+from itertools import combinations
+import networkx as nx
+
+
+def suffix_trie(text: str) -> nx.prefix_tree:
+    text = text + "$"
+    suffixes = [text[-i:] for i in range(1, len(text) + 1)]
+    return nx.prefix_tree(suffixes)
+
+
+if __name__ == "__main__":
+    suffix_trie("test")
