dcd04de39f8dacc861a5bf6b0524e81c1ed4e46b
carrollpaul <57120760+TheBoilerman@users.noreply.github.com>
2021-11-08 16:46:38 -0800

    Add files

--- bioinformatics_4/week1/trie_matching.py
+++ bioinformatics_4/week1/trie_matching.py
@@ -1,38 +1,42 @@
-import os
+import typing
+from collections import defaultdict
 from networkx.generators.trees import prefix_tree
 
 
-def prefix_trie_matching(text: str, trie: prefix_tree):
-    root, nil = 0, -1
-    for v in trie.successors(root):
-        char = 0
-        while v != nil:
-            node_val = str(trie.nodes[v]["source"])
-            if node_val == text[char]:
-                print(node_val)
-                char += 1
-                v = next(trie.successors(v))
-        """
-        while v != nil:
-            print(v, str(trie.nodes[v]["source"]))
-            v = next(trie.successors(v))
-    
-    for char in text:
-        for v
-    for edge in edges:
-        if edge[1] == -1:
-            pattern = []
-            for v in trie.predecessors(-1):
-                while v != 0:
-                    prefix = ""
-                    prefix = str(trie.nodes[v]["source"]) + prefix
-                    v = next(trie.predecessors(v))
-                pattern.append(prefix)
-            print(pattern)"""
+def recover_path(node: int, trie: prefix_tree) -> str:
+    for v in trie.predecessors(node):
+        prefix = str(trie.nodes[node]["source"])
+        while v != 0:
+            prefix = str(trie.nodes[v]["source"]) + prefix
+            v = next(trie.predecessors(v))
+    return prefix
+
+
+def prefix_trie_matching(text: str, trie: prefix_tree, source: int = 0):
+    if list(trie.successors(source)) == [-1]:
+        return recover_path(source, trie)
+
+    for child in list(trie.successors(source)):
+        if text[0] == trie.nodes[child]["source"]:
+            source = child
+            text = text[1:]
+            return prefix_trie_matching(text, trie, source)
+
+
+def trie_matching(text: str, trie: prefix_tree) -> typing.List["int"]:
+    index = 0
+    matches = defaultdict(list)
+    while True:
+        if index == len(text):
+            return matches
+        match = prefix_trie_matching(text[index:], trie)
+        if match != None:
+            matches[match].append(index)
+        index += 1
 
 
 if __name__ == "__main__":
     patterns = ["ATCG", "GGGT"]
     text = "AATCGGGTTCAATCGGGGT"
     trie = prefix_tree(patterns)
-    prefix_trie_matching(text, trie)
+    print(trie_matching(text, trie))
