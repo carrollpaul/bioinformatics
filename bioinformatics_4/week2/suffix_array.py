4d2ef0f88d853161c7ae414c4df3bda63e090afc
carrollpaul <57120760+TheBoilerman@users.noreply.github.com>
2021-11-10 11:08:36 -0800

    Add docstring

--- bioinformatics_4/week2/suffix_array.py
+++ bioinformatics_4/week2/suffix_array.py
@@ -3,6 +3,9 @@ import os
 
 
 def suffix_array(text: str) -> typing.List["int"]:
+    """
+    Return a list of indices in lexigraphical order of all suffixes in text, starting with $.
+    """
     if text[-1] != "$":
         text = text + "$"
     suffixes = {text[-i:]: len(text) - i for i in range(1, len(text) + 1)}
